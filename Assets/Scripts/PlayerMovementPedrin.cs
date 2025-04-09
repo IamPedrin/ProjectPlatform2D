4d67e6f8d4fc6ad4c14cf046d6b8b3e996bf6417
Pedro Luiz <pedrin.luiz22@gmail.com>
2025-04-09 18:11:03 -0300

    Coyote Time e Jump InputBuffer funcionando

--- Assets/Scripts/PlayerMovementPedrin.cs
+++ Assets/Scripts/PlayerMovementPedrin.cs
@@ -1,28 +1,32 @@
 using System;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+
+public class PlayerMovementPedrin : MonoBehaviour
 {
-    //Components
     private Rigidbody2D rb;
 
+    [Header("InputAction")]
+    public InputActionReference movementReference;
+    public InputActionReference jumpReference;
+
     [Header("Movement")]
-    private Vector2 moveInput; //Recebe o input do jogador se movimentando
     public float moveSpeed = 5f; //Velocidade maxima que o jogador pode alcançar
     public float acceleration;
     public float decceleration;
     public float velPower;
     [Range(0, 1)] public float frictionAmount;
+    private Vector2 moveInput; //Recebe o input do jogador se movimentando
 
     [Header("Jump")]
     public float jumpForce = 10f;
-    public int maxJumps = 1;
-    private int jumpsRemaining;
     [Range(0, 0.9f)] public float jumpCutMultiplier;
-    public float coyoteTime;
-    public float jumpInputBufferTimer;
+    public float coyoteTime = 0.2f;
+    public float jumpBufferTimer = 0.2f;
+    private bool doubleJump;
     bool isJumping = false;
 
     [Header("Gravity")]
@@ -31,21 +35,16 @@ public class PlayerMovement : MonoBehaviour
     public float maxFallSpeed;
 
 
-
     [Header("GroundCheck")]
     public Transform groundCheckPos;
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
     public LayerMask groundLayer;
     bool isGrounded;
-
     //Variaveis de controle e tempo
     public bool IsFacingRight { get; private set; }
-    public float LastOnGroundTime { get; private set; }
-    public float LastJumpTime { get; private set; }
-    public float LastPressedJumpTime { get; private set; }
+    public float LastGroundTime { get; private set; }
     public float CoyoteTimer { get; private set; }
-    public float JumpBufferTimer { get; private set; }
-
+    public float JumpBufferCounter { get; private set; }
 
     void Start()
     {
@@ -55,24 +54,25 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        CheckTimers();
-        Gravity();
-        Friction();
-        FlipSprite();
         GroundCheck();
-    }
-    private void FixedUpdate()
-    {
-        Movement();
+        TimersCheck();
+        FlipSprite();
     }
 
-    //Função para movimentação horizontal do jogador utilizando o novo InputSystem
-    public void Move(InputAction.CallbackContext context)
+    void FixedUpdate()
+    {
+        Movement();
+        InitiateJump();
+        Friction();
+        Gravity();
+    }
+
+    #region Movement
+    public void MovementInput(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
     }
 
-    //Melhorando a movimentação
     private void Movement()
     {
         //Calcula a direcao e valocidade maxima que queremos
@@ -88,10 +88,11 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(movement * Vector2.right);
     }
 
+
     //Função para implementar um mecanismo de atrito e desacelerar o jogador enquanto estiver no chao
     private void Friction()
     {
-        if (LastOnGroundTime > 0 && Mathf.Abs(moveInput.x) < 0.01f)
+        if (LastGroundTime > 0 && Mathf.Abs(moveInput.x) < 0.01f)
         {
             //Quantidade de força de atrito será aplicado no jgogador
             float amount = Mathf.Min(Mathf.Abs(rb.linearVelocity.x), Mathf.Abs(frictionAmount));
@@ -102,7 +103,105 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    //Função para inverter o sprite do jogador de acordo com sua direção
+    #endregion
+
+    #region Jump
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            JumpBufferCounter = jumpBufferTimer;
+        }
+        else if (context.canceled)
+        {
+            if (rb.linearVelocity.y > 0)
+            {
+                rb.AddForce(Vector2.down * rb.linearVelocity.y * (1 - jumpCutMultiplier), ForceMode2D.Impulse);
+            }
+            LastGroundTime = 0;
+        }
+    }
+
+    private void InitiateJump()
+    {
+        if (JumpBufferCounter > 0 && (isGrounded || CoyoteTimer > 0f))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            JumpBufferCounter = 0f;
+            CoyoteTimer = 0f;
+            isJumping = true;
+        }
+    }
+
+    #endregion
+
+    #region Gravity
+
+    //Função para aumentar a gravidade enquanto o jogador está caindo
+    private void Gravity()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+
+            rb.gravityScale = baseGravity * fallGravityMultiplier;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+
+        }
+    }
+
+    #endregion
+
+    #region Checkers
+
+    //Verifica se o jogador esta em colisão com o chão
+    private void GroundCheck()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            isGrounded = true;
+            isJumping = false;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (isGrounded && !jumpReference.action.WasPressedThisFrame())
+        {
+            doubleJump = false;
+        }
+    }
+
+    private void TimersCheck()
+    {
+
+        LastGroundTime = Mathf.Max(0, LastGroundTime - Time.deltaTime);
+        JumpBufferCounter = Mathf.Max(0, JumpBufferCounter - Time.deltaTime);
+
+        #region Coyote Checkers
+        if (!isGrounded)
+        {
+            CoyoteTimer = Mathf.Max(0, CoyoteTimer - Time.deltaTime);
+        }
+        else
+        {
+            CoyoteTimer = coyoteTime;
+        }
+
+        if (jumpReference.action.WasReleasedThisFrame())
+        {
+            CoyoteTimer = 0f;
+        }
+        #endregion
+    }
+    #endregion
+
+    #region Auxiliar
     private void FlipSprite()
     {
         //Verifica se esta olhando para a direita e tem um movimento para a esquerda
@@ -116,87 +215,10 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    //Função para aplicar o pulo ao jogador
-    public void Jump(InputAction.CallbackContext context)
-    {
-        if (jumpsRemaining > 0)
-        {
-
-            if (context.performed)
-            {
-                JumpBufferTimer = jumpInputBufferTimer;
-                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                isJumping = true;
-                jumpsRemaining--;
-                LastOnGroundTime = 0;
-                LastJumpTime = 0;
-
-            }
-            else if (context.canceled)
-            {
-                JumpBufferTimer = jumpInputBufferTimer;
-                rb.AddForce(Vector2.down * rb.linearVelocity.y * (1 - jumpCutMultiplier), ForceMode2D.Impulse);
-                isJumping = true;
-                jumpsRemaining--;
-                LastJumpTime = 0;
-                LastOnGroundTime = 0;
-            }
-        }
-    }
-
-    #region Checkers 
-    //Verifica se o jogador esta em colisão com o chão
-    private void GroundCheck()
-    {
-        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
-        {
-            isGrounded = true;
-            isJumping = false;
-            jumpsRemaining = maxJumps;
-            LastOnGroundTime = coyoteTime;
-        }
-        else
-        {
-            isGrounded = false;
-        }
-    }
-
-    //Função para aumentar a gravidade enquanto o jogador está caindo
-    private void Gravity()
-    {
-        if (rb.linearVelocity.y < 0)
-        {
-            rb.gravityScale = baseGravity * fallGravityMultiplier;
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -maxFallSpeed));
-
-        }
-        else
-        {
-            rb.gravityScale = baseGravity;
-        }
-    }
-
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.white;
         Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
     }
-
-    private void CheckTimers()
-    {
-        LastOnGroundTime -= Time.deltaTime;
-        LastJumpTime -= Time.deltaTime;
-        JumpBufferTimer -= Time.deltaTime;
-
-        if (!isGrounded)
-        {
-            CoyoteTimer -= Time.deltaTime;
-        }
-        else
-        {
-            CoyoteTimer = coyoteTime;
-        }
-    }
-
     #endregion
 }
